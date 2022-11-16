7e613b84a1b4b9123d5367b016f115334cc0f501
DreadfulCrazy <lucasospina97@gmail.com>
2022-11-16 11:33:59 -0500

    Translating the background to a correct position

--- index.js
+++ index.js
@@ -4,63 +4,67 @@ const canvasContext = canvas.getContext("2d"); // Where passing what kind of API
 canvas.width = 1024;
 canvas.height = 576;
 
+const scaledCanvas = {
+    width: canvas.width / 4,
+    height: canvas.height / 4
+}
 const gravity = 0.5;
 
 class Sprite {
-  constructor({ position, imageSrc }) {
-    this.position = position;
-    this.image = new Image();
-    this.image.src = imageSrc;
-  }
-
-  draw() {
-    if (!this.image) return
-    canvasContext.drawImage(this.image, this.position.x, this.position.y);
-  }
-
-  update() {
-    this.draw()
-  }
+    constructor({ position, imageSrc }) {
+        this.position = position;
+        this.image = new Image();
+        this.image.src = imageSrc;
+    }
+    
+    draw() {
+        if (!this.image) return
+        canvasContext.drawImage(this.image, this.position.x, this.position.y);
+    }
+    
+    update() {
+        this.draw()
+    }
 }
 
 class Player {  
-  constructor(position) {
-    this.position = position;
-    this.velocity = {
-      x: 0,
-      y: 1,
-    };
-    this.height = 100;
-  }
-
-  draw() {
-    canvasContext.fillStyle = "red";
-    canvasContext.fillRect(this.position.x, this.position.y, 100, this.height);
-  }
-
-  update() {
-    this.draw();
-
-    this.position.y += this.velocity.y;
-    this.position.x += this.velocity.x;
-
-    /**
-     * Logic when the player hit the bottom of canvas
-     */
-    if (this.position.y + this.height + this.velocity.y < canvas.height)
-      this.velocity.y += gravity;
-    else this.velocity.y = 0;
-  }
+    constructor(position) {
+        this.position = position;
+        this.velocity = {
+            x: 0,
+            y: 1,
+        };
+        this.height = 100;
+    }
+    
+    draw() {
+        canvasContext.fillStyle = "red";
+        canvasContext.fillRect(this.position.x, this.position.y, 100, this.height);
+    }
+    
+    update() {
+        this.draw();
+        
+        this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
+        
+        /**
+         * Logic when the player hit the bottom of canvas
+         */
+        if (this.position.y + this.height + this.velocity.y < canvas.height)
+        this.velocity.y += gravity;
+        else this.velocity.y = 0;
+    }
 }
 
 const player1 = new Player({
-  x: 0,
-  y: 0,
+    x: 0,
+    y: 0,
 });
 
 const player2 = new Player({
-  x: 200,
-  y: 0,
+    x: 200,
+    y: 0,
 });
 
 const background = new Sprite({
@@ -70,6 +74,7 @@ const background = new Sprite({
     },
     imageSrc: './img/background.png'
 })
+console.log(-background.image.height + scaledCanvas.height);
 
 /**
  * https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
@@ -84,6 +89,12 @@ function animate() {
    */
   canvasContext.save()
   canvasContext.scale(4, 4)
+  // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/translate
+  /**
+   * Translate the origin position background.position {x: 0, y: 0}
+   * to the unities passed through the the function
+   */
+  canvasContext.translate(0, -background.image.height + scaledCanvas.height)
   background.update()
   canvasContext.restore()
 
